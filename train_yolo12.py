b1887e759686cfe66c0f693bf0002abd28cb3cd6
EnkhAmar <enkhamar1220@gmail.com>
2025-03-24 14:16:42 +0800

    change

--- train_yolo12.py
+++ train_yolo12.py
@@ -8,9 +8,13 @@ model = YOLO("yolo12m.pt")
 train_results = model.train(
     data="data/yolo/data.yaml",
     epochs=100,
+    time=None,
+    patience=100,
     imgsz=640,
     device="cuda" if torch.cuda.is_available() else "cpu",
-    batch=16 
+    batch=16,
+    iou=0.6,
+    conf=0.001,
 )
 
-model.export(format="pt")
+model.export(format="torchscript")
